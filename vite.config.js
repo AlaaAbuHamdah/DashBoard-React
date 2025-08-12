d7ff535ab7d1babb6f69c4e69b2b2cbb20b86a26
AlaaAbuHamdah <alaa.yhashem@gmail.com>
2025-08-12 23:34:08 +0300

    Initial1

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/DashBoard-React/"
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/DashBoard-React/"
+// });
+
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    base: command === "serve" ? "/" : "/DashBoard-React/",
+  };
 });
